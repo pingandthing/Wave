0b42237bf1f339232276507a0a1caba1d2fe0cf4
Jonathan Davis Samuel <jone.bala@gmail.com>
2020-09-05 16:55:46 -0500

    Create Room

--- wave/src/components/Landing.vue
+++ wave/src/components/Landing.vue
@@ -2,29 +2,74 @@
   <v-container fluid>
     <v-row align="center" justify="center">
       <v-col align="center" justify="center">
-        <v-btn>Host a room</v-btn>
+        <v-dialog v-model="hostDialog" persistent>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+        >
+        Host a Room
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title class="headline">Create Room</v-card-title>
+        <v-card-text>dsfdsfdsfsdfsdfdsfdsg.</v-card-text>
+        <v-text-field
+           label="Room Name"
+           placeholder="Create Room Here"
+           outlined
+           v-model = "newRoomName"
+         ></v-text-field>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="error " text @click="hostDialog = false">Cancel</v-btn>
+          <v-btn color="success" text @click="createRoomHandler()">Create</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
       </v-col>
       <v-col align="center" justify="center">
         <v-btn>Join a room</v-btn>
       </v-col>
     </v-row>
+    {{this.rooms}}
   </v-container>
 </template>
 
 <script>
+const api = "http://65d6f0cc736b.ngrok.io/"
 import axios from "axios";
 export default {
   name: "Landing",
 
   data: () => ({
     //
-    rooms: null
+    rooms: null,
+    newRoomName: null,
+    hostDialog: false
   }),
+  methods:{
+    createRoomHandler(){
+      axios.get(api+"createRoom?room=" +this.newRoomName ).then(response => {
+        console.log(response)
+        this.rooms = response.data.rooms;
+        console.log(this)
+      })
+      .catch(error => {
+        console.log(error);
+        // this.errored = true
+      });
+      this.hostDialog = true;
+    }
+  },
   mounted() {
     axios
-      .get("http://65d6f0cc736b.ngrok.io/")
+      .get(api)
       .then(response => {
-        this.rooms = response.rooms;
+        console.log(response)
+        this.rooms = response.data.rooms;
+        console.log(this)
       })
       .catch(error => {
         console.log(error);
