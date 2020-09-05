b3376ed05f68eb6c89079c6b178e62e7121eba1b
Jonathan Davis Samuel <jone.bala@gmail.com>
2020-09-05 13:57:51 -0500

    Join/Host buttons

--- wave/src/App.vue
+++ wave/src/App.vue
@@ -2,23 +2,7 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        Wave
       </div>
 
       <v-spacer></v-spacer>
@@ -34,19 +18,19 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <Landing />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Landing from "./components/Landing";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    Landing
   },
 
   data: () => ({
---
+++ wave/src/components/Landing.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container fluid>
+    <v-row align="center" justify="center">
+      <v-col align="center" justify="center">
+        <v-btn>Host a room</v-btn>
+      </v-col>
+      <v-col align="center" justify="center">
+        <v-btn>Join a room</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
