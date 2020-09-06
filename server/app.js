82558edf5a3d3da18cd08c2f31b0e35e885790df
Jonathan Davis Samuel <jone.bala@gmail.com>
2020-09-05 21:46:14 -0500

    Websocket init

--- server/app.js
+++ server/app.js
@@ -1,42 +1,97 @@
-const http = require('http');
-const url = require('url');
-
-const hostname = '127.0.0.1';
+const http = require("http");
+const url = require("url");
+var WebSocketServer = require("websocket").server;
+const hostname = "127.0.0.1";
 const port = 3000;
 
-rooms = ["General"]
+rooms = ["General"];
 
-const server = http.createServer((req, res) => { //runs everytime its loaded
-  ret = {}
+const server = http.createServer((req, res) => {
+  //runs everytime its loaded
+  ret = {};
   res.statusCode = 200;
 
-   const queryObject = url.parse(req.url,true);
-   console.log(queryObject)
+  const queryObject = url.parse(req.url, true);
+  console.log(queryObject);
 
-   if(queryObject.pathname == "/createRoom"){
-    var newRoom = queryObject.query.room
-    if(rooms.includes(newRoom)){
-      console.log("Room already exists")
-      ret.creationStatus = false
-    }
-    else{
+  if (queryObject.pathname == "/createRoom") {
+    var newRoom = queryObject.query.room;
+    if (rooms.includes(newRoom)) {
+      console.log("Room already exists");
+      ret.creationStatus = false;
+    } else {
       //create room
-      rooms.push(newRoom)
-      ret.creationStatus = true
+      rooms.push(newRoom);
+      ret.creationStatus = true;
       //go from there
     }
-   }
+  } else if (queryObject.pathname == "/joinRoom") {
+    var joinRoom = queryObject.query.room;
+    if (rooms.includes(newRoom)) {
+      console.log("Room exists");
+      ret.creationStatus = true;
+    } else {
+      ret.creationStatus = false;
+      //go from there
+    }
+  }
 
-  console.log(rooms)
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  	res.setHeader('Access-Control-Request-Method', '*');
-  	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-  	res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Content-Type', 'application/json');
-  ret.rooms = rooms
+  console.log(rooms);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Content-Type", "application/json");
+  ret.rooms = rooms;
   res.end(JSON.stringify(ret));
 });
 
-server.listen(port, hostname, () => { //runs when the server is ready
+wsServer = new WebSocketServer({
+  httpServer: server,
+  // You should not use autoAcceptConnections for production
+  // applications, as it defeats all standard cross-origin protection
+  // facilities built into the protocol and the browser.  You should
+  // *always* verify the connection's origin and decide whether or not
+  // to accept it.
+  autoAcceptConnections: false,
+});
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+wsServer.on("request", function (request) {
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log(
+      new Date() + " Connection from origin " + request.origin + " rejected."
+    );
+    return;
+  }
+
+  var connection = request.accept("echo-protocol", request.origin);
+  console.log(new Date() + " Connection accepted.");
+  connection.on("message", function (message) {
+    if (message.type === "utf8") {
+      console.log("Received Message: " + message.utf8Data);
+      connection.sendUTF(message.utf8Data);
+    } else if (message.type === "binary") {
+      console.log(
+        "Received Binary Message of " + message.binaryData.length + " bytes"
+      );
+      connection.sendBytes(message.binaryData);
+    }
+  });
+  connection.on("close", function (reasonCode, description) {
+    console.log(
+      new Date() + " Peer " + connection.remoteAddress + " disconnected."
+    );
+  });
+});
+
+server.listen(port, hostname, () => {
+  //runs when the server is ready
   console.log(`Server running at http://${hostname}:${port}/`);
 });
--- wave/src/components/Landing.vue
+++ wave/src/components/Landing.vue
@@ -50,6 +50,7 @@
 <script>
 const api = "http://65d6f0cc736b.ngrok.io/";
 import axios from "axios";
+var W3CWebSocket = require("websocket").w3cwebsocket;
 export default {
   name: "Landing",
 
@@ -96,6 +97,35 @@ export default {
         // this.errored = true
       });
     // .finally(() => this.loading = false)
+    // eslint-disable-line
+    var client = new W3CWebSocket("ws://localhost:3000/", "echo-protocol");
+
+    client.onerror = function() {
+      console.log("Connection Error");
+    };
+
+    client.onopen = function() {
+      console.log("WebSocket Client Connected");
+
+      function sendNumber() {
+        if (client.readyState === client.OPEN) {
+          var number = Math.round(Math.random() * 0xffffff);
+          client.send(number.toString());
+          setTimeout(sendNumber, 1000);
+        }
+      }
+      sendNumber();
+    };
+
+    client.onclose = function() {
+      console.log("echo-protocol Client Closed");
+    };
+
+    client.onmessage = function(e) {
+      if (typeof e.data === "string") {
+        console.log("Received: '" + e.data + "'");
+      }
+    };
   }
 };
 </script>
