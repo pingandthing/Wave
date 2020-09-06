b3c2af5050ca244404c9711537556f6b2d09d784
Jonathan Davis Samuel <jone.bala@gmail.com>
2020-09-06 09:38:03 -0500

    Port

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const http = require("http");
 const url = require("url");
 var WebSocketServer = require("websocket").server;
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 rooms = ["General"];
 connections = [];
