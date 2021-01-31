f4e755e147353ecbc1168e6db2ccec3ab2b971bc
Jonathan <jone.bala@gmail.com>
2021-01-31 15:01:22 -0600

    'deploy'

---
+++ precache-manifest.5ddd505bed7e6215f534ee356a985907.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abc77e4ab13d080b648fb505d7dd638",
+    "url": "/Wave/404.html"
+  },
+  {
+    "revision": "6349e5950062974a2ce2",
+    "url": "/Wave/css/app.6960b1b1.css"
+  },
+  {
+    "revision": "0cf284d277e6e699f75f",
+    "url": "/Wave/css/chunk-vendors.684d8931.css"
+  },
+  {
+    "revision": "01e5b5fc306eedd91d14ba7a81b573db",
+    "url": "/Wave/img/logo.01e5b5fc.png"
+  },
+  {
+    "revision": "72f5d83a6fcb756a1d0a5d296eeca0d5",
+    "url": "/Wave/img/wave.72f5d83a.gif"
+  },
+  {
+    "revision": "6413c034551f6b6a27e08289b92f3a75",
+    "url": "/Wave/index.html"
+  },
+  {
+    "revision": "9f8ef5dbae2ecad71889",
+    "url": "/Wave/js/about.1741a65f.js"
+  },
+  {
+    "revision": "6349e5950062974a2ce2",
+    "url": "/Wave/js/app.8bc50e06.js"
+  },
+  {
+    "revision": "0cf284d277e6e699f75f",
+    "url": "/Wave/js/chunk-vendors.c68fdb6a.js"
+  },
+  {
+    "revision": "21feabed61ad0ba5cebb7ea924fa6196",
+    "url": "/Wave/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Wave/robots.txt"
+  }
+]);
